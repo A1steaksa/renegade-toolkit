15e264701a94dd6751728cff3a438e88ef1038be
John Schneider <8369707+A1steaksa@users.noreply.github.com>
2026-04-15 09:11:39 -0500

    Clean up imports and debug function

--- src/extension.ts
+++ src/extension.ts
@@ -2,19 +2,11 @@ import * as vscode from 'vscode';
 import { LanguageSwitcher } from './language-switcher/language-switcher';
 import { LuaImportManager } from './lua-import-manager/import-manager';
 import { LuaScanner } from './file-scanner/lua-scanner';
-import { LuaImportableClassScanner } from './lua-import-manager/importable-class-scanner';
-import { LuaImportableCache } from './lua-import-manager/importable-class-cache';
-import { TextUtils } from './utils/text-utils';
-import { LuaClass } from './lua-import-manager/importables/lua-class';
-import { CodeGen } from './code-gen/code-gen';
 import { LuaClassCreation } from './lua-class-creation/lua-class-creation';
-import { CppScanner } from './file-scanner/cpp-scanner';
-import { HeaderScanner } from './file-scanner/header-scanner';
 
 export const config = vscode.workspace.getConfiguration( "renegade-toolkit" );
 
 export function activate( context: vscode.ExtensionContext ) {
-    
     // Set up scanners
     // HeaderScanner.initialize( context );
     // CppScanner.initialize( context );
@@ -26,14 +18,8 @@ export function activate( context: vscode.ExtensionContext ) {
 
     LuaClassCreation.initialize( context );
 
-    const debugDisposable = vscode.commands.registerCommand( "renegade-toolkit.debug", () => {
-        const document = vscode.window.activeTextEditor?.document;
-        if( document === undefined ){
-            return;
-        }
-
-        console.log( vscode.workspace.asRelativePath( document.uri ) );
-
+    const debugDisposable = vscode.commands.registerCommand( "renegade-toolkit.debug", async () => {
+        
     } );
     context.subscriptions.push( debugDisposable );
 
